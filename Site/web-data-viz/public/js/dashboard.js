14fa5649ed2661a510a299569b2102439475348e
luccaboliveira <lucca.oliveira@sptech.school>
2025-04-01 20:16:14 -0300

    fix: ajustes na estilização do modal de notificações

---
+++ Site/web-data-viz/public/js/dashboard.js
@@ -0,0 +1,11 @@
+function abrirModal() {
+    var modalNotificacao = document.getElementById("modal_notificacao");
+
+    modalNotificacao.style.display = "flex";
+}
+
+function fecharModal() {
+    var modalNotificacao = document.getElementById("modal_notificacao");
+
+    modalNotificacao.style.display = "none";
+}
