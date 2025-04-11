c37286c565bb5936f2783eaeacb56c85999c8392
LucasRodriguesCartaxo <lucas.cartaxo@sptech.school>
2025-04-11 16:53:56 -0300

    S3 com java

--- leitor-excel/src/main/java/school/sptech/Main.java
+++ leitor-excel/src/main/java/school/sptech/Main.java
@@ -1,34 +1,41 @@
 package school.sptech;
 
-import java.io.IOException;
+import software.amazon.awssdk.services.s3.S3Client;
+
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-//        String nomeArquivo = "Dados_PI_99Bairros.xlsx";
-        String nomeArquivo = "SPDadosCriminais_2025.xlsx";
+    public static void main(String[] args) {
+        try {
+            // Nome do bucket e do arquivo
+            String bucketName = "test-vida";
+            String objectKey = "SPDadosCriminais_2025.xlsx";
 
-        // Carregando o arquivo excel
-        Path caminho = Path.of(nomeArquivo);
-        InputStream arquivo = Files.newInputStream(caminho);
+            // Criando cliente S3
+            S3Provider provider = new S3Provider();
+            S3Client s3Client = provider.getS3Client();
 
-        // Extraindo os livros do arquivo
-        LeitorExcel leitorExcel = new LeitorExcel();
-        List<Dado> dadosExtraidos = leitorExcel.extrairDados(nomeArquivo, arquivo, 0);
+            // Fazendo o download do arquivo como InputStream
+            S3Downloader downloader = new S3Downloader(s3Client);
+            InputStream arquivo = downloader.baixarArquivo(bucketName, objectKey);
 
-        // Fechando o arquivo após a extração
-        arquivo.close();
+            // Extração dos dados via Apache POI
+            LeitorExcel leitorExcel = new LeitorExcel();
+            List<Dado> dados = leitorExcel.extrairDados(objectKey, arquivo, 0);
 
-        System.out.println("Dados extraídos:");
-        for (Dado dado : dadosExtraidos) {
-            System.out.println(dado.toString());
+            // Exibir os dados extraídos
+            System.out.println("Dados extraídos do S3:");
+            for (Dado dado : dados) {
+                System.out.println(dado);
+            }
+
+            // Fechar o stream após uso
+            arquivo.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        System.out.println(dadosExtraidos.size());
     }
 }
