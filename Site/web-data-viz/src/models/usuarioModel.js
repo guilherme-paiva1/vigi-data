682addafebad55331ef73a2359b3a9e88bbee935
guilherme-paiva1 <guilherme.paiva@sptech.school>
2025-04-30 23:08:55 -0300

    corrigindo consulta no banco

--- Site/web-data-viz/src/models/usuarioModel.js
+++ Site/web-data-viz/src/models/usuarioModel.js
@@ -3,8 +3,8 @@ var database = require("../database/config")
 function entrar(matricula, senha) {
     var instrucaoSql = `
         SELECT u.idUsuario, u.nome, u.email, u.matricula, u.perfil, s.nome AS superior
-            FROM Usuario AS u
-            JOIN Usuario AS s ON u.fkSupervisor = s.idUsuario
+            FROM usuario AS u
+            JOIN usuario AS s ON u.fkSupervisor = s.idUsuario
                 WHERE u.matricula = '${matricula}' AND u.senha = '${senha}';
     `;
 
@@ -13,7 +13,7 @@ function entrar(matricula, senha) {
 
 function cadastrar(nome, email, matricula, perfil, idSuperior, senha) {
     var instrucaoSql = `
-        INSERT INTO Usuario (nome, email, matricula, perfil, superior, senha) VALUES
+        INSERT INTO usuario (nome, email, matricula, perfil, superior, senha) VALUES
 	        ('${nome}', '${email}', '${matricula}', '${perfil}', ${idSuperior}, '${senha}');
     `;
 
