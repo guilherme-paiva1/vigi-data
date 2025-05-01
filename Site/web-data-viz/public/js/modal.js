9026b346eec5701e858a247a3a12839d547482b2
guilherme-paiva1 <guilherme.paiva@sptech.school>
2025-05-01 14:13:25 -0300

    feature: adicionando a responsividade da navbar nas outras telas + validacoes

--- Site/web-data-viz/public/js/modal.js
+++ Site/web-data-viz/public/js/modal.js
@@ -16,20 +16,21 @@ function sair() {
   window.location.href = "../index.html";
 }
 
-var btnUsuario = document.getElementById("btn_menu");
+var btnMenu = document.getElementById("btn_menu");
 var navbar = document.querySelector(".navbar");
 
-
-btnUsuario.addEventListener("click", function () {
+btnMenu.addEventListener("click", function () {
   if (navbar.classList.contains("show")) {
       navbar.classList.remove("show"); // fecha a navbar
+      modalNotificacao.style.display = "none";
+      modalUsuario.style.display = "none";
   } else {
       navbar.classList.add("show"); // abre a navbar
   }
 });
 
 document.addEventListener("click", function (event) {
-  if (!btnUsuario.contains(event.target) && !navbar.contains(event.target)) {
+  if (!btnMenu.contains(event.target) && !navbar.contains(event.target)) {
       navbar.classList.remove("show"); // fecha a navbar se clicar fora
   }
 });
