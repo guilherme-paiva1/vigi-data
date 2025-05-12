df50cd822853d52224e3722b83b54b8fc314e5c7
beagareno <bhreno.barboza@sptech.school>
2025-05-12 20:20:51 -0300

    Rota de listagem de investigação por delegado

--- Site/web-data-viz/app.js
+++ Site/web-data-viz/app.js
@@ -1,5 +1,5 @@
 var ambiente_processo = 'producao';
-//var ambiente_processo = 'desenvolvimento';
+// var ambiente_processo = 'desenvolvimento';
 
 var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
 // Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
@@ -17,7 +17,7 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var investigacaoRouter = require("./src/routes/usuarios");
+var investigacaoRouter = require("./src/routes/investigacao");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
