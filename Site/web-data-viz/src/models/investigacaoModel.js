630dead0a20af7e86682e6ccf2d4704950d5d586
guilherme-paiva1 <guilherme.paiva@sptech.school>
2025-05-12 20:35:28 -0300

    feature: adicionando inserção no historico simultaneamente a criacao da investigacao

--- Site/web-data-viz/src/models/investigacaoModel.js
+++ Site/web-data-viz/src/models/investigacaoModel.js
@@ -9,6 +9,31 @@ function cadastrar(titulo, descricao, localidade, dt_investigacao,  incidencia) 
     return database.executar(instrucaoSql);
 }
 
+function registrarHistoricoDoDelegado(fkDelegado, titulo, descricao, localidade, dt_investigacao) {
+    var intrucaoSqlRequisicao = `
+        SELECT idInvestigacao FROM investigacao 
+            WHERE 
+                dt_investigacao = '${dt_investigacao}' 
+                AND status_atual = 'pendente'
+                AND titulo = '${titulo}'
+                AND descricao = '${descricao}'
+                AND localidade = '${localidade}';
+    `;
+
+    database.executar(intrucaoSqlRequisicao)
+        .then((resultado) => {
+            if (resultado.length > 0) {
+                var idInvestigacao = resultado[0].idInvestigacao;
+                var queryHistorico = `
+                    INSERT INTO historico_investigacao (fkDelegado, fkInvestigacao, criador) 
+                        VALUES (${fkDelegado}, ${idInvestigacao}, 1);
+                `;
+                return database.executar(queryHistorico);
+            }
+        });
+}
+
 module.exports = {
-    cadastrar
+    cadastrar,
+    registrarHistoricoDoDelegado
 };
