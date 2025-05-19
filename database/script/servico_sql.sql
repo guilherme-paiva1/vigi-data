ff72454d3295996551b309420a57d39710b0c223
guilherme-paiva1 <guilherme.paiva@sptech.school>
2025-05-19 20:22:59 -0300

    resolvendo de vez

--- database/script/servico_sql.sql
+++ database/script/servico_sql.sql
@@ -1,5 +1,5 @@
 -- Criação do usuário
-CREATE USER 'svc_vida'@'%' IDENTIFIED BY 'urubu100';
+CREATE USER IF NOT EXISTS 'svc_vida'@'%' IDENTIFIED BY 'urubu100';
 
 -- Concessão de permissões ao usuário
 GRANT ALL PRIVILEGES ON vida.* TO 'svc_vida'@'%';
