1fa5b102930ea23d209e750e37d3653e2b8fa1c4
guilherme-paiva1 <guilherme.paiva@sptech.school>
2025-05-19 19:32:18 -0300

    feature: corrigindo queries de insercao

--- database/script/queries_vida.sql
+++ database/script/queries_vida.sql
@@ -1,10 +1,9 @@
-INSERT INTO Usuario (nome, email, matricula, perfil, fkSupervisor, senha) VALUES
-	('Paulo Roberto', 'paulo@gmail.com', '1234567', 'delegado', 1, '123');
+INSERT INTO usuario (nome, email, matricula, perfil, fkSupervisor, ativo, senha) VALUES
+	('Paulo Roberto', 'paulo@gmail.com', '1234567', 'delegado', 1, 1,  '123');
     
-INSERT INTO Usuario (nome, email, matricula, perfil, fkSupervisor, senha) VALUES
-	('João Paulo', 'joao@gmail.com', '7654321', 'policial', 1, '123');
+INSERT INTO usuario (nome, email, matricula, perfil, fkSupervisor, ativo, senha) VALUES
+	('João Paulo', 'joao@gmail.com', '7654321', 'policial', 1, 1, '123');
     
-
 SELECT u.idUsuario, u.nome, u.email, u.matricula, u.perfil, s.nome AS superior
             FROM Usuario AS u
             JOIN Usuario AS s 
