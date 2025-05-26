4a1482794b3c3985a8036cde0f787e4d69cce7ee
guilherme-paiva1 <guilherme.paiva@sptech.school>
2025-05-26 17:22:22 -0300

    subindo alteração pra incluir rota da dash

--- Site/web-data-viz/app.js
+++ Site/web-data-viz/app.js
@@ -18,6 +18,7 @@ var app = express();
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var investigacaoRouter = require("./src/routes/investigacao");
+var ocorrenciaRouter = require("./src/routes/ocorrencia");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,6 +29,7 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/investigacao", investigacaoRouter);
+app.use("/ocorrencia", ocorrenciaRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
