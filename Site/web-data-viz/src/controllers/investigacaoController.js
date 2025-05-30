363d99ccfc1e80c146749d29a158a28c9aad3fd6
guilherme-paiva1 <guilherme.paiva@sptech.school>
2025-05-30 19:37:31 -0300

    fix: menores correções em rotas e afins

--- Site/web-data-viz/src/controllers/investigacaoController.js
+++ Site/web-data-viz/src/controllers/investigacaoController.js
@@ -114,40 +114,6 @@ function visualizarInvestigacaoPorId(req, res) {
         );
 }
 
-function exibir(req, res) {
-    var id = req.body.idServer;
-
-    if (id == undefined) {
-        res.status(400).send("id inválido.");
-    } else {
-
-        investigacoesModel.exibir(id)
-            .then(
-                function (resultadoInvestigacoes) {
-                    if (resultadoInvestigacoes.length >= 1) {
-                        res.json({
-                            id_usuario: resultadoInvestigacoes[0].id_usuario,
-                            nome: resultadoInvestigacoes[0].nome,
-                            email: resultadoInvestigacoes[0].email,
-                            id: resultadoInvestigacoes[0].id,
-                            perfil: resultadoInvestigacoes[0].perfil,
-                            superior: resultadoInvestigacoes[0].superior,
-                        });
-                    } else if (resultadoInvestigacoes.length == 0) {
-                        res.status(403).send("id inválido.");
-                    }
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log("\nHouve um erro ao exibir as investigações! Erro: ", erro.sqlMessage);
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    }
-
-}
-
 function excluirInvestigacao(req, res) {
     var id = req.body.idServer;
 
@@ -155,7 +121,7 @@ function excluirInvestigacao(req, res) {
         res.status(400).send("id inválido.");
     } else {
 
-        investigacoesModel.excluirInvestigacao(id)
+        investigacaoModel.excluirInvestigacao(id)
             .then(
                 function (resultado) {
                     res.json(resultado);
