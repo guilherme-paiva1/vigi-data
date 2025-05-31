f03ab8edf052ed15948af6777ffb71c975a2ba55
guilherme-paiva1 <guilherme.paiva@sptech.school>
2025-05-31 14:48:37 -0300

    feature: adicionando travas + navegacao do policial pra dashboard customizada dele

--- Site/web-data-viz/public/js/modal.js
+++ Site/web-data-viz/public/js/modal.js
@@ -149,24 +149,6 @@ function perfilSession() {
 
   if (sessionStorage.PERFIL_USUARIO == "delegado") {
     conteudoModal.innerHTML += `
-    <a href="#">
-      <div class="conteudo-opcao">
-            <div class="icon">
-                <svg  width="30px" height="30px"  viewBox="0 -0.5 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M16.5 9C16.5 11.2091 14.7091 13 12.5 13C10.2909 13 8.5 11.2091 8.5 9C8.5 6.79086 10.2909 5 12.5 5C13.5609 5 14.5783 5.42143 15.3284 6.17157C16.0786 6.92172 16.5 7.93913 16.5 9Z" stroke="#1b1b1d" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M5.5 18.9999C9.78787 16.3408 15.2121 16.3408 19.5 18.9999" stroke="#000000" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
-            </div>
-            <p>Meu perfil</p>
-        </div>
-    </a>
-
-      <a href="#">
-        <div class="conteudo-opcao">
-            <div class="icon">
-                <svg width="27px" height="27px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="style=linear"> <g id="setting"> <path id="vector" d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="#000000" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path> <path id="vector_2" d="M2 12.88V11.12C2 10.08 2.85 9.22 3.9 9.22C5.71 9.22 6.45 7.94 5.54 6.37C5.02 5.47 5.33 4.3 6.24 3.78L7.97 2.79C8.76 2.32 9.78 2.6 10.25 3.39L10.36 3.58C11.26 5.15 12.74 5.15 13.65 3.58L13.76 3.39C14.23 2.6 15.25 2.32 16.04 2.79L17.77 3.78C18.68 4.3 18.99 5.47 18.47 6.37C17.56 7.94 18.3 9.22 20.11 9.22C21.15 9.22 22.01 10.07 22.01 11.12V12.88C22.01 13.92 21.16 14.78 20.11 14.78C18.3 14.78 17.56 16.06 18.47 17.63C18.99 18.54 18.68 19.7 17.77 20.22L16.04 21.21C15.25 21.68 14.23 21.4 13.76 20.61L13.65 20.42C12.75 18.85 11.27 18.85 10.36 20.42L10.25 20.61C9.78 21.4 8.76 21.68 7.97 21.21L6.24 20.22C5.33 19.7 5.02 18.53 5.54 17.63C6.45 16.06 5.71 14.78 3.9 14.78C2.85 14.78 2 13.92 2 12.88Z" stroke="#000000" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g> </g></svg>
-            </div>
-            <p>Configurações</p>
-        </div>
-      </a>
-
       <a href="./dashboard.html">
         <div class="conteudo-opcao">
             <div class="icon">
@@ -200,16 +182,13 @@ function perfilSession() {
   }
 
   if (sessionStorage.PERFIL_USUARIO == "policial") {
+    a_investigacoes = document.getElementById("a_investigacoes");
+    a_investigacoes.style.display = "none";
+    a_usuarios = document.getElementById("a_usuarios");
+    a_usuarios.style.display = "none";
+    a_notificacoes = document.getElementById("a_notificacoes");
+    a_notificacoes.style.display = "none";
     conteudoModal.innerHTML += `
-    <a href="#">
-      <div class="conteudo-opcao">
-            <div class="icon">
-                <svg  width="30px" height="30px"  viewBox="0 -0.5 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M16.5 9C16.5 11.2091 14.7091 13 12.5 13C10.2909 13 8.5 11.2091 8.5 9C8.5 6.79086 10.2909 5 12.5 5C13.5609 5 14.5783 5.42143 15.3284 6.17157C16.0786 6.92172 16.5 7.93913 16.5 9Z" stroke="#1b1b1d" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M5.5 18.9999C9.78787 16.3408 15.2121 16.3408 19.5 18.9999" stroke="#000000" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
-            </div>
-            <p>Meu perfil</p>
-        </div>
-    </a>
-
     <a href="./investigacoes.html">
       <div class="conteudo-opcao">
         <div class="icon">
@@ -218,16 +197,14 @@ function perfilSession() {
         <p>Minhas investigações</p>
       </div>
     </a>
-
-      <a href="#">
-        <div class="conteudo-opcao">
-            <div class="icon">
-                <svg width="27px" height="27px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="style=linear"> <g id="setting"> <path id="vector" d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="#000000" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path> <path id="vector_2" d="M2 12.88V11.12C2 10.08 2.85 9.22 3.9 9.22C5.71 9.22 6.45 7.94 5.54 6.37C5.02 5.47 5.33 4.3 6.24 3.78L7.97 2.79C8.76 2.32 9.78 2.6 10.25 3.39L10.36 3.58C11.26 5.15 12.74 5.15 13.65 3.58L13.76 3.39C14.23 2.6 15.25 2.32 16.04 2.79L17.77 3.78C18.68 4.3 18.99 5.47 18.47 6.37C17.56 7.94 18.3 9.22 20.11 9.22C21.15 9.22 22.01 10.07 22.01 11.12V12.88C22.01 13.92 21.16 14.78 20.11 14.78C18.3 14.78 17.56 16.06 18.47 17.63C18.99 18.54 18.68 19.7 17.77 20.22L16.04 21.21C15.25 21.68 14.23 21.4 13.76 20.61L13.65 20.42C12.75 18.85 11.27 18.85 10.36 20.42L10.25 20.61C9.78 21.4 8.76 21.68 7.97 21.21L6.24 20.22C5.33 19.7 5.02 18.53 5.54 17.63C6.45 16.06 5.71 14.78 3.9 14.78C2.85 14.78 2 13.92 2 12.88Z" stroke="#000000" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g> </g></svg>
-            </div>
-            <p>Configurações</p>
+    <a href="./dashboard-policial.html">
+      <div class="conteudo-opcao">
+        <div class="icon">
+          <svg width="26px" height="26px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21 21H7.8C6.11984 21 5.27976 21 4.63803 20.673C4.07354 20.3854 3.6146 19.9265 3.32698 19.362C3 18.7202 3 17.8802 3 16.2V3M15 10V17M7 13V17M19 5V17M11 7V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
         </div>
-      </a>
-
+        <p>Minha dashboard</p>
+      </div>
+    </a>
       <hr>
 
       <a href="#">
@@ -284,7 +261,7 @@ async function carregarNotificacoes() {
               icon = icon_sucesso;
               break;
           }
-            estruturaHTML += `
+          estruturaHTML += `
               <div class="notificacao" id="notificacao_${notificacao.idNotificacao}">
                 <div class="icon">
                   <img src="${icon}" alt="${notificacao.tipo}" style="width:32px;height:32px;">
--- Site/web-data-viz/public/js/notificacoes.js
+++ Site/web-data-viz/public/js/notificacoes.js
@@ -1,3 +1,4 @@
+if (sessionStorage.PERFIL_USUARIO == "policial") window.location.href = "./investigacoes.html";
 window.onload = function () {
     carregarPoliciais();
     carregarAlertas();
