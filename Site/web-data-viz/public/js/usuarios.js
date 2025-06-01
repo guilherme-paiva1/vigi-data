034d2af6ceacc5d2ff07c64e11356b6291a0ed5f
beagareno <bhreno.barboza@sptech.school>
2025-06-01 16:00:02 -0300

    Função movida de investigacoes.js pra usuarios.js

--- Site/web-data-viz/public/js/usuarios.js
+++ Site/web-data-viz/public/js/usuarios.js
@@ -209,3 +209,28 @@ function confirmarExclusaoUsuario() {
 function carregarInformacoesDesempenhoUsuario(idUsuario) {
     console.log("carregar informações do desempenho do usuário com id: " + idUsuario);
 }
+
+function visualizarDesempenhoPolicial() {
+    var id = sessionStorage.ID_USUARIO;
+
+    fetch(`/visualizarDesempenhoPolicial/${id}`, {
+        method: "GET"
+    })
+        .then(function (resposta) {
+            if (resposta.ok) {
+                return resposta.json();
+            } else if (resposta.status === 204) {
+                throw new Error("ID inválido.");
+            } else if (resposta.status === 403) {
+                throw new Error("Nenhuma investigação encontrada para este mês.");
+            } else {
+                throw new Error("Erro ao buscar o desempenho.");
+            }
+        })
+        .then(function (dados) {
+            console.log("Desempenho recebido:", dados);
+        })
+        .catch(function (erro) {
+            console.error("Erro:", erro.message);
+        });
+}
